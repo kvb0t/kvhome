43d25b51fa28e0603cd1ba71fcac602e9b7abd2e
kev <kev@iam.kvb0t.com>
2021-11-07 21:38:06 -0800

    Small formatting changes and Resume link added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, render_template
 
 
@@ -7,6 +8,10 @@ app = Flask(__name__,template_folder="templates",static_folder="static")
 @app.route("/")
 def home():
     return render_template('home.html')
+
+@app.route("/resume/")
+def resume():
+    return flask.redirect("https://rebrand.ly/kevsilver_resume")
 
 
 if __name__ == '__main__':
